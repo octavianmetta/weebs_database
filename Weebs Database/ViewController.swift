469816e7757efb15671f5849aa57470571023181
Octavian Metta Wisnu <octavian.wardhana@indosystem.com>
2020-04-01 12:48:52 +0700

    Initial Commit

---
+++ Weebs Database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weebs Database
+//
+//  Created by Octavian Metta Wisnu on 01/04/20.
+//  Copyright © 2020 Octavian Metta Wisnu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
